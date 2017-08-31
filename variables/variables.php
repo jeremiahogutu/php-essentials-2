85501ae2b0a00f7375107e40255c73fde9c2634f
Jeremiah Ogutu <jeremiahogutu@protonmail.com>
2017-08-31 13:58:41 -0400

    added variables

---
+++ variables/variables.php
@@ -0,0 +1,27 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Variables</title>
+</head>
+<body>
+<?php
+    $var1 = 10;
+    echo $var1;
+
+    echo "<br />";
+
+    $var1 = 100;
+    echo $var1;
+
+    echo "<br />";
+
+    $var2 = "Hello World";
+    echo $var2;
+?>
+
+</body>
+</html>
