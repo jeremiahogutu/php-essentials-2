83b997319b8c9130ede8306017368d44d78e1e69
Jeremiah Ogutu <jeremiahogutu@protonmail.com>
2017-09-03 18:11:17 -0400

    continue in a loop

---
+++ continue/continue.php
@@ -0,0 +1,46 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Continue</title>
+</head>
+<body>
+<?php
+    for ($count = 0; $count <= 10; $count++) {
+        if ($count % 2 == 0) {
+            continue;
+        }
+        echo $count . ", ";
+    }
+?>
+<br />
+<?php
+    $count = 0;
+    while($count <= 10) {
+        if ($count == 5) {
+            $count++;
+            continue;
+        }
+        echo $count . ", ";
+        $count++;
+    }
+?>
+<br />
+<?php // loop inside a loop with continue
+    for ($i = 0; $i <= 5; $i++) {
+        if ($i % 2 == 0) {
+            continue(1);
+        }
+        for ($k = 0; $k <= 5; $k++) {
+            if ($k == 3) {
+                continue(2);
+            }
+            echo $i . "-" . $k . "<br />";
+        }
+    }
+?>
+</body>
+</html>
